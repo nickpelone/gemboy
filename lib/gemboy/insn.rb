6190207669d10627216228991813d9c11bfc1c2d
Nick Pelone <nick.pelone@gmail.com>
2018-03-29 10:16:31 -0400

    Begin meta-definition of instructions
    Many can be implemented in a generic method that is just told the
    operand registers and values to work on based on info we can store about
    each opcode.
    The dispatch for instructions in the CPU will decode the opcode, look up
    its entry in OPCODES, and then `send` to the value of the `method` key.
    
    That's the plan, right now, anyway. There is a concern about lookup time
    plus the overhead of method lookup, but that will need to be witnessed
    in the real world before more decisions can be made.

--- lib/gemboy/insn.rb
+++ lib/gemboy/insn.rb
@@ -1,5 +1,46 @@
+# frozen-string-literal: true
 module Gemboy
   module Instructions
+    OPCODES = {
+      0x00 => {
+        method: :nop
+      },
+      # 8-bit loads
+
+      # TODO LD nn,n
+      # (opcodes 06, 0E, 16, 1E, 26, 2E)
+
+      0x7F => {
+        method: :ld_r1_r2
+        params: [:@a, :@a],
+        mtime: 1
+      },
+
+      0x078 => {
+        method: :ld_r1_r2,
+        params: [:@a, :@b],
+        mtime: 1
+      },
+
+      0x79 => {
+        method: :ld_r1_r2,
+        params: [:@a, :@c],
+        mtime: 1
+      },
+
+      0x7A => {
+        method: :ld_r1_r2,
+        params: [:@a, :@d],
+        mtime: 1
+      },
+
+      0x7B => {
+        method: :ld_r1_r2,
+        params: [:@a, :@e],
+        mtime: 1
+      }
+    }
+    # opcode 00
     def nop
       # 1 M-time
       @m = 1
